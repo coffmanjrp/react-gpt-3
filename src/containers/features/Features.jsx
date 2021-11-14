d61bc573cd5a9912d89ef47fd89c9a65db0323a3
coffmanjrp <coffmanjrp@gmail.com>
2021-11-13 23:18:13 -0800

    Code Features component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,7 +1,24 @@
+import { Feature } from '../../components';
+import { featuresData } from './data';
 import './features.scss';
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div id="features" className="gpt3__features section__padding">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today &amp; Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map(({ id, title, text }) => (
+          <Feature key={id} title={title} text={text} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
