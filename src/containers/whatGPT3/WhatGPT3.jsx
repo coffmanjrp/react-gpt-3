d5d554a1e986edbae1f2c3cab03e71e5fbda6722
coffmanjrp <coffmanjrp@gmail.com>
2021-11-13 18:30:53 -0800

    Code What is GP3 Section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,26 @@
+import { whatGPT3Feature, whatGPT3Container } from './data';
+import { Feature } from '../../components';
 import './whatGPT3.scss';
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div id="wgpt3" className="gpt3__whatgpt3 section__margin">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title={whatGPT3Feature.title} text={whatGPT3Feature.text} />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        {whatGPT3Container.map(({ id, title, text }) => (
+          <Feature key={id} title={title} text={text} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
---
+++ src/containers/whatGPT3/data.js
@@ -0,0 +1,22 @@
+export const whatGPT3Feature = {
+  title: 'What is GPT-3',
+  text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.',
+};
+
+export const whatGPT3Container = [
+  {
+    id: 1,
+    title: 'Chatbots',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought.',
+  },
+  {
+    id: 2,
+    title: 'Knowledgebase',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b',
+  },
+  {
+    id: 3,
+    title: 'Education',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b',
+  },
+];
