e218d7cc9b1440c9908b24fc9e76deabeef11d22
coffmanjrp <coffmanjrp@gmail.com>
2021-11-13 17:24:18 -0800

    Code Brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,35 +1,7 @@
-import { atlassian, dropbox, google, shopify, slack } from './imports';
+import brands from './imports';
 import './brand.scss';
 
 const Brand = () => {
-  const brands = [
-    {
-      id: 1,
-      src: google,
-      alt: 'Google',
-    },
-    {
-      id: 2,
-      src: slack,
-      alt: 'Slack',
-    },
-    {
-      id: 3,
-      src: atlassian,
-      alt: 'Atlassian',
-    },
-    {
-      id: 4,
-      src: dropbox,
-      alt: 'Dropbox',
-    },
-    {
-      id: 5,
-      src: shopify,
-      alt: 'Shopify',
-    },
-  ];
-
   return (
     <div className="gpt3__brand section__padding">
       {brands.map(({ id, src, alt }) => (
--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -4,4 +4,32 @@ import google from '../../assets/google.png';
 import shopify from '../../assets/shopify.png';
 import slack from '../../assets/slack.png';
 
-export { atlassian, dropbox, google, shopify, slack };
+const brands = [
+  {
+    id: 1,
+    src: google,
+    alt: 'Google',
+  },
+  {
+    id: 2,
+    src: slack,
+    alt: 'Slack',
+  },
+  {
+    id: 3,
+    src: atlassian,
+    alt: 'Atlassian',
+  },
+  {
+    id: 4,
+    src: dropbox,
+    alt: 'Dropbox',
+  },
+  {
+    id: 5,
+    src: shopify,
+    alt: 'Shopify',
+  },
+];
+
+export default brands;
