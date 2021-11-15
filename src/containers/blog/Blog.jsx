4eea6ba334d0045595161b8db1d3934f3f722ae1
coffmanjrp <coffmanjrp@gmail.com>
2021-11-14 22:08:00 -0800

    Code Blog component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,33 @@
+import { Article } from '../../components';
+import articles from './imports';
 import './blog.scss';
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const newArticles = [...articles.slice(0, 0), ...articles.slice(0 + 1)];
+
+  return (
+    <div id="blog" className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={articles[0].imgUrl}
+            date={articles[0].date}
+            title={articles[0].title}
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          {newArticles.map(({ id, title, date, imgUrl }) => (
+            <Article key={id} imgUrl={imgUrl} date={date} title={title} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,40 @@
+import blog01 from '../../assets/blog01.png';
+import blog02 from '../../assets/blog02.png';
+import blog03 from '../../assets/blog03.png';
+import blog04 from '../../assets/blog04.png';
+import blog05 from '../../assets/blog05.png';
+
+const articles = [
+  {
+    id: 1,
+    date: 'Nov 15, 2021',
+    title: 'GPT-3 and Open AI is the future. Let us exlore how it is?',
+    imgUrl: blog01,
+  },
+  {
+    id: 2,
+    date: 'Nov 01, 2021',
+    title: 'Could This Report Be The Definitive Answer To Your WEB Dev?',
+    imgUrl: blog02,
+  },
+  {
+    id: 3,
+    date: 'Oct 13, 2021',
+    title: 'Rules Not To Follow About AI',
+    imgUrl: blog03,
+  },
+  {
+    id: 4,
+    date: 'Oct 05, 2021',
+    title: 'Got Stuck? Try These Tips To Streamline Your NATURAL LANGUAGE',
+    imgUrl: blog04,
+  },
+  {
+    id: 5,
+    date: 'Sep 26, 2021',
+    title: 'Here Is A Quick Cure For GPT-3 OPEN AI',
+    imgUrl: blog05,
+  },
+];
+
+export default articles;
